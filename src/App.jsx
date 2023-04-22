ab5f68b534641feb830c2687396b1b27b6e3e23e
sonuv2201 <sonuv.2201@gmail.com>
2023-04-22 15:03:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import { BrowserRouter, Route, Routes,Link } from 'react-router-dom'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>DEMO</h2>
+      <BrowserRouter>
+        <header>
+          <Link to='/'>Home</Link> |
+          <Link to='/about'>&nbsp;About</Link> |
+          <Link to='/contact'>&nbsp;Contact</Link>
+        </header>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+        </Routes>
+      </BrowserRouter>
+
+    </>
+  )
+}
+
+export default App
